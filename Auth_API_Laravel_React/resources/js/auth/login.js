8f0d6edda9a260175fb1b1a9e74f92c0eb924105
Dennis-Ivan <ivanchavs7@gmail.com>
2022-07-18 08:12:21 +0800

    Second commit

---
+++ Auth_API_Laravel_React/resources/js/auth/login.js
@@ -0,0 +1,16 @@
+import './login.css';
+import Content from "../components/content";
+import LoginForm from './loginForm';
+import '../bootstrap';
+
+
+const Login = () => {
+    return (
+        <div>
+            <Content />
+        </div>
+    )
+}
+
+export default Login;
+
